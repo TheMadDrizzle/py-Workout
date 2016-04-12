62d559b67eab63699e2272f760459ee5a26bb7d6
Drew <davis.drews@gmail.com>
2016-04-11 20:18:48 -0400

    Minor Updates

--- workout.py
+++ workout.py
@@ -4,15 +4,16 @@
 # Drew Davis
 #
 
-# GLOBALS
+# IMPORT
+from sys import argv
 
-# DEBUG
-
-# MAIN
+# GLOBAL
+saveFile, openFile = argv
 
 
 # WORKOUT
 class Exersize(object):
+    # Add index for auto advance upon completion of week 12?
     def __init__(self, name, week, mastery):
         self.name = name
         self.mastery = mastery
@@ -26,6 +27,11 @@ THIRTY = { 1: '3x6', 2: '5x6', 3: '3x12', 4: '3x6', 5: '4x12', 6: '4x18', 7: '5x
 FIFTEEN = { 1: '3x3', 2: '5x3', 3: '3x6', 4: '3x3', 5: '4x6', 6: '4x9', 7: '5x9', 8: '4x5' }
 TEN = { 1: '3x2', 2: '5x2', 3: '3x4', 4: '3x2', 5: '4x4', 6: '4x6', 7: '5x6', 8: '5x3' }
 FIVE = { 1: '3x1', 2: '5x1', 3: '3x2', 4: '3x1', 5: '4x2', 6: '4x3', 7: '5x3', 8: '5x2' }
+
+# List builder proto?
+#class List(object):
+#    def __init__(self)
+#
 
 def main():
     week = 1
